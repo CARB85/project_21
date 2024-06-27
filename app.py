4111de8f61d05bb87752ed0a9378b41264acc9e1
CARB85 <173071167+CARB85@users.noreply.github.com>
2024-06-27 13:48:00 -0400

    actualizar cambios

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ X = np.random.normal(0, 10, size=(100, 2))
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 6))
 
-ax[0].hist(X[::, 0], color='red', alpha = 0.4)
+ax[0].hist(X[::, 0], 
+           color='red', 
+           alpha = 0.4, 
+           bins = 50)
 ax[0].spines['top'].set_visible(False)
 ax[0].spines['right'].set_visible(False)
 ax[0].set_title("X1", size= 14)
-ax[1].hist(X[::, 1], color='orange', alpha = 0.6)
+ax[1].hist(X[::, 1], 
+           color='orange', 
+           alpha = 0.6, 
+           bins= 50)
 ax[1].spines['top'].set_visible(False)
 ax[1].spines['right'].set_visible(False)
 ax[1].set_title("X2", size= 14)
